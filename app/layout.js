f76026c5e5d53cd888869d5f63fc996fafc162d7
ajiaron <aaronjiang2001@gmail.com>
2024-01-21 12:26:47 -0800

    added gtag to metadata

--- app/layout.js
+++ app/layout.js
@@ -41,13 +41,20 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
         <Head>
-        {metadata.scripts.map((script, index) => (
-          script.src ? (
-            <script key={index} src={script.src} async={script.async}></script>
-          ) : (
-            <script key={index} dangerouslySetInnerHTML={{ __html: script.innerHTML }}></script>
-          )
-        ))}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-JP2GFR7QN8"`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JP2GFR7QN8');
+            `,
+          }}
+        />
         </Head>
       <body className={inter.className}>
 
--- dist/server/webpack-runtime.js
+++ dist/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ee8a79993fbe11d3")
+/******/ 		__webpack_require__.h = () => ("27ef30439b1b7737")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
